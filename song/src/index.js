3eb97f35ce658a651c525a10d7a19664dba79652
songyu <1402975007@qq.com>
2020-11-24 10:28:33 +0800

    flx:添加基本框架

--- song/src/index.js
+++ song/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import SiderDemo from './App';
 import reportWebVitals from './reportWebVitals';
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SiderDemo />
   </React.StrictMode>,
   document.getElementById('root')
 );
